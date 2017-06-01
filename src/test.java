64d1b0bf719e6722c4e0b2384808b302c26793b5
Arya Pradipta <aryapradipta9@gmail.com>
2017-06-01 10:01:59 +0700

    Eksplorasi Github API dan HttpUrlConnection

--- src/test.java
+++ src/test.java
@@ -1,8 +1,11 @@
 import com.google.gson.Gson;
 import java.io.*;
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.List;
+import java.util.Map;
+import javax.ws.rs.GET;
+import org.apache.http.client.methods.HttpGet;
 
 /**
  * Created by 13515017 / Putu Arya Pradipta.
@@ -13,12 +16,35 @@ public class test {
 
 
   public static void main(String[] args) {
+    HttpGet temp;
+
     HttpURLConnection con = null;
     StringBuilder str = new StringBuilder();
     URL url = null;
     try {
       url = new URL("https://api.github.com/users/aryapradipta9/repos");
       con = (HttpURLConnection) url.openConnection();
+      con.setRequestMethod("GET");
+      Map<String, List<String>> map = con.getHeaderFields();
+
+      System.out.println("Printing Response Header...\n");
+
+      for (Map.Entry<String, List<String>> entry : map.entrySet()) {
+        System.out.println("Key : " + entry.getKey()
+            + " ,Value : " + entry.getValue());
+      }
+
+      System.out.println("\nGet Response Header By Key ...\n");
+      String server = con.getHeaderField("Server");
+
+      if (server == null) {
+        System.out.println("Key 'Server' is not found!");
+      } else {
+        System.out.println("Server - " + server);
+      }
+
+      System.out.println("\n Done");
+
       InputStream in = new BufferedInputStream(con.getInputStream());
       BufferedReader bfr = new BufferedReader(new InputStreamReader(in));
       String line;
@@ -26,22 +52,28 @@ public class test {
         str.append(line);
       }
 
-    } catch (MalformedURLException e) {
-      e.printStackTrace();
     } catch (IOException e) {
       e.printStackTrace();
     } finally {
       con.disconnect();
     }
-    Gson gson = null;
-    gson.fromJson(str.toString(),Temp.class);
-    System.out.print(str);
+    Gson gson = new Gson();
+    //Temp[] ints2 = gson.fromJson("[{\"name\":\"WAWA\",\"urls\":\"http://code.google.com/p/google-gson/\",\"Inner\": {\"in\":5}},{\"name\":\"WAWAK\",\"urls\":\"http://code.google.com/p/google-gson/\",\"Inner\": {\"in\":4}}]", Temp[].class);
+    Temp[] ints2 = gson.fromJson(str.toString(),Temp[].class);
+    System.out.print(ints2[0].name);
 
 
   }
 
   private static class Temp {
-    private String name;
+    public String name;
+
+    //public inclass Inner;
+    public static class inclass {
+      public int in;
+      public inclass() {
+      }
+    }
     public Temp(){
       
     }
